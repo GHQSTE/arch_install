d253ae94873dfe6c8a35931eb6c616039f63b547
Wasiqur Rahman <>
2022-04-16 23:57:36 +0600

    update

--- arch_install.sh
+++ arch_install.sh
@@ -40,7 +40,6 @@ sed -i 's/^#color/color/' /etc/pacman.conf
 ln -sf /usr/share/zoneinfo/Asia/Dhaka /etc/localtime ; hwclock --systohc
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/^#ja_JP.UTF-8 UTF-8/ja_JP.UTF-8 UTF-8/' /etc/locale.gen
-sed -i 's/^#bn_BD UTF-8/bn_BD UTF-8/' /etc/locale.gen
 locale-gen ; echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "Enter hostname: " ; read -r hostname
 echo "$hostname" > /etc/hostname
